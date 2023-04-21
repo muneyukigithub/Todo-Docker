40aba9f23c6a26f017fdf084128cc86c50e7f47f
test <example@test.com>
2023-04-21 13:36:39 +0900

    settings_local削除

--- backend/django/Todo_DRF/settings/settings_local.py
+++
@@ -1,4 +0,0 @@
-# settings_localはgitにあげたくない情報を記載
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-30f%rn0$5w+3s3vbe_lv+_9)cau2&3x%p0#2!(@y4ge+s$_cl+'
